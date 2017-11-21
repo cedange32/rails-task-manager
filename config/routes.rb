e023c4e4217f1ad7c1b9932c602769523bf4a70c
charles edange <charlesedange@hotmail.com>
2017-11-21 16:04:56 +0100

    done, reste a faire les refacto

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+
 end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read
+    # 1. read all
+  # get "tasks", to: "tasks#index"
+  #   # 2. read one
+  # get "tasks/:id", to: "tasks#show", as: 'task'
